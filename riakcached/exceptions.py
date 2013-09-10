b9b3eddc65200b0795a2e5e9e75213a77133fbd9
brettlangdon <brett@blangdon.com>
2013-09-09 23:40:26 -0400

    add empty docstrings for exceptions

--- riakcached/exceptions.py
+++ riakcached/exceptions.py
@@ -1,23 +1,46 @@
 class RiakcachedException(Exception):
+    """
+    """
     pass
+
 
 class RiakcachedBadRequest(RiakcachedException):
+    """
+    """
     pass
+
 
 class RiakcachedNotFound(RiakcachedException):
+    """
+    """
     pass
+
 
 class RiakcachedServiceUnavailable(RiakcachedException):
+    """
+    """
     pass
+
 
 class RiakcachedPreconditionFailed(RiakcachedException):
+    """
+    """
     pass
+
 
 class RiakcachedConflict(RiakcachedException):
+    """
+    """
     pass
+
 
 class RiakcachedTimeout(RiakcachedException):
+    """
+    """
     pass
 
+
 class RiakcachedConnectionError(RiakcachedException):
+    """
+    """
     pass
