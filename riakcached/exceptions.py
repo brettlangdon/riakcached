f24f8b1878267d59dfaef95dc7da0e17442274ce
brettlangdon <brett@blangdon.com>
2013-09-12 19:16:09 -0400

    add documentation for riakcached.exceptions

--- riakcached/exceptions.py
+++ riakcached/exceptions.py
@@ -1,46 +1,60 @@
 class RiakcachedException(Exception):
-    """
+    """Base class for Riakcached Exceptions
     """
     pass
 
 
 class RiakcachedBadRequest(RiakcachedException):
-    """
+    """Exception that is raised for unexpected HTTP 400 status responses
+
+    Inherits from :class:`riakcached.exceptions.RiakcachedException`
     """
     pass
 
 
 class RiakcachedNotFound(RiakcachedException):
-    """
+    """Exception that is raised for unexpected HTTP 204 status responses
+
+    Inherits from :class:`riakcached.exceptions.RiakcachedException`
     """
     pass
 
 
 class RiakcachedServiceUnavailable(RiakcachedException):
-    """
+    """Exception that is raised for unexpected HTTP 503 status responses
+
+    Inherits from :class:`riakcached.exceptions.RiakcachedException`
     """
     pass
 
 
 class RiakcachedPreconditionFailed(RiakcachedException):
-    """
+    """Exception that is raised for unexpected HTTP 412 status responses
+
+    Inherits from :class:`riakcached.exceptions.RiakcachedException`
     """
     pass
 
 
 class RiakcachedConflict(RiakcachedException):
-    """
+    """Exception that is raised for unexpected HTTP 409 status responses
+
+    Inherits from :class:`riakcached.exceptions.RiakcachedException`
     """
     pass
 
 
 class RiakcachedTimeout(RiakcachedException):
-    """
+    """Exception that is raised when pool requests takes too long
+
+    Inherits from :class:`riakcached.exceptions.RiakcachedException`
     """
     pass
 
 
 class RiakcachedConnectionError(RiakcachedException):
-    """
+    """Exception that is raised when pool requests raises an `HTTPError`
+
+    Inherits from :class:`riakcached.exceptions.RiakcachedException`
     """
     pass
